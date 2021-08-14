bf4348ffe0c0798ef27c0502e9dddc883ce09c08
rafael <rafadev21@gmail.com>
2021-08-14 18:31:13 -0300

    1 desafio

---
+++ mediaEmC.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <locale.h>
+
+int main (){
+
+	//variaveis
+	float nota1,nota2,nota3,nota4,somar;
+	
+	//Entrada de dados
+
+	printf("Digite a 1° nota :");
+	scanf("%f",&nota1);	
+	printf("\nDigite o 2  nota :");
+	scanf("%f",&nota2);	
+	printf("\nDigite o 3 nota:");
+	scanf("%f",&nota3);	
+	printf("\nDigite o 4 nota:");
+	scanf("%f",&nota4);
+	
+	somar = (nota1+nota2+nota3+nota4) /4;
+	
+	printf("\nO resultado da media  é :%f",somar);
+	
+
+		
+	
+	
+
+}
