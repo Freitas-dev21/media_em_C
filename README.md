5dd62a5f1f81f411964b20da1d84500a2b64a0f7
rafael <rafadev21@gmail.com>
2021-08-15 00:12:57 -0300

    metodo setlocale

---
+++ README.md
@@ -0,0 +1,1 @@
+# media_em_C
--- mediaEmC.cpp
+++ mediaEmC.cpp
@@ -3,6 +3,7 @@
 #include <locale.h>
 
 int main (){
+	setlocale(LC_ALL, "Portuguese");
 
 	//variaveis
 	float nota1,nota2,nota3,nota4,somar;
@@ -11,11 +12,11 @@ int main (){
 
 	printf("Digite a 1° nota :");
 	scanf("%f",&nota1);	
-	printf("\nDigite o 2  nota :");
+	printf("\nDigite o 2° nota :");
 	scanf("%f",&nota2);	
-	printf("\nDigite o 3 nota:");
+	printf("\nDigite o 3° nota:");
 	scanf("%f",&nota3);	
-	printf("\nDigite o 4 nota:");
+	printf("\nDigite o 4° nota:");
 	scanf("%f",&nota4);
 	
 	somar = (nota1+nota2+nota3+nota4) /4;
